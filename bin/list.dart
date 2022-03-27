edad0ad6c533eae69dfe0d1adfffd26eb415003e
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-28 02:18:41 +0700

    add list.dart

--- bin/list.dart
+++ bin/list.dart
@@ -1,11 +1,25 @@
 void main(List<String> args) {
   List<String> name = ['Minh', 'Duc', 'Duong'];
+  name.forEach((element) {
+      print(element);
+    }); 
 
-  name.addAll(['Khang', 'Nhat', 'Luan']);
+  //name.add('Nguyen');
+  // name.addAll(['Khang', 'Nhat', 'Luan']);
+  // print(name);
 
-  name.insert(name.length, "Cao");
+  // name.insert(name.length, "Cao");
+  // name.insertAll(1, ["Cao", 'Nguyen', 'Nhat']);
+  // print(name);
 
-  name.remove('Cao');
-
-  print(name);
+  // name.remove('Cao');
+  // name.removeAt(1);
+  // name.removeLast();
+  // name.removeRange(1, 2);
+  // name.clear();
+  //  print(name);
+  
+  // name[1] = 'Nguyen';
+  // name.replaceRange(1, 2, ['Linh', 'Nhat']);
+  // print(name);
 }
