a41f1e69bff0ef0e032a70f475ebdce17bf36008
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-28 17:37:02 +0700

    add control_flow

---
+++ bin/control_flow.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> args) {
+  // //if_else
+  // bool isRaining = Random().nextBool();
+  // bool isGoingOut;
+  // if (isRaining) {
+  //   isGoingOut = false;
+  //   print(isGoingOut);
+  // } else {
+  //   isGoingOut = true;
+  //   print(isGoingOut);
+  // }
+
+  // //switch
+  // print("Input month: ");
+  // int month = int.parse(stdin.readLineSync()!);
+  // int day;
+  // switch (month) {
+  //   case 1:
+  //   case 3:
+  //   case 5:
+  //   case 7:
+  //   case 8:
+  //   case 10:
+  //   case 12:
+  //     day = 31;
+  //     break;
+  //   case 4:
+  //   case 6:
+  //   case 9:
+  //   case 11:
+  //     day = 30;
+  //     break;
+  //   case 2:
+  //     day = 28;
+  //     break;
+  //   default:
+  //     day = 0;
+  // }
+
+  //for_loop
+  // for (int i = 0; i < 10; i++) {
+  //   print(i);
+  // }
+
+  //for each
+  // var list = [1, 2, 3, 4, 5];
+  // for (var index in list) {
+  //   print(index);
+  // }
+
+  //while_loop
+  // var i = 1;
+  // while (i<5) {
+  //   print(i);
+  //   i++;
+  // }
+    
+  //do_while_loop
+  // bool isRaining = Random().nextBool();
+  // do {
+  //   if (isRaining) {
+  //     print('It is raining');
+  //   } else {
+  //     print('It is not raining');
+  //   }
+  // } while (false);
+}
