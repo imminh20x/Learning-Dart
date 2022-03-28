68b9bdd28efc0c20dd53af72b6f0b815b2c2e8d7
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-28 21:35:32 +0700

    update oop.dart

--- bin/oop.dart
+++ bin/oop.dart
@@ -7,20 +7,43 @@ class Student {
   var gpa = 0.0;
   var isGraduated = false;
   var isSuspended = false;
+
+//constructor
+  Student(this.name, this.age, this.major, this.gpa, this.isGraduated,
+      this.isSuspended);
+
+  // Student(Student newStudent) {
+  //   name = newStudent.name;
+  //   age = newStudent.age;
+  //   major = newStudent.major;
+  //   gpa = newStudent.gpa;
+  //   isGraduated = newStudent.isGraduated;
+  //   isSuspended = newStudent.isSuspended;
+  // }
+//method
+  void printInfo() {
+    print('Name: $name');
+    print('Age: $age');
+    print('Major: $major');
+    print('GPA: $gpa');
+    print('IsGraduated: $isGraduated');
+    print('IsSuspended: $isSuspended');
+  }
 }
 
 void main(List<String> args) {
-  var student = Student();
-  student.name = 'Minh';
-  student.age = 20;
-  student.major = ['Software Engineering'];
-  student.gpa = 3.5;
-  student.isGraduated = false;
-  student.isSuspended = false;
-  print(student.name);
-  print(student.age);
-  print(student.major);
-  print(student.gpa);
-  print(student.isGraduated);
-  print(student.isSuspended);
+  var student = Student('Minh', 21, ['Software Engineering'], 3.5, true, false);
+  // student.name = 'Minh';
+  // student.age = 20;
+  // student.major = ['Software Engineering'];
+  // student.gpa = 3.5;
+  // student.isGraduated = false;
+  // student.isSuspended = false;
+  // print(student.name);
+  // print(student.age);
+  // print(student.major);
+  // print(student.gpa);
+  // print(student.isGraduated);
+  // print(student.isSuspended);
+  student.printInfo();
 }
