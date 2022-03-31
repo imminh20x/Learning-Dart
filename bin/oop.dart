a0eeacbdcf01ca07a8d6ec6d313619d2ad1d6feb
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-31 20:40:57 +0700

    update oop.dart

--- bin/oop.dart
+++ bin/oop.dart
@@ -12,14 +12,6 @@ class Student {
   Student(this.name, this.age, this.major, this.gpa, this.isGraduated,
       this.isSuspended);
 
-  // Student(Student newStudent) {
-  //   name = newStudent.name;
-  //   age = newStudent.age;
-  //   major = newStudent.major;
-  //   gpa = newStudent.gpa;
-  //   isGraduated = newStudent.isGraduated;
-  //   isSuspended = newStudent.isSuspended;
-  // }
 //method
   void printInfo() {
     print('Name: $name');
@@ -33,17 +25,5 @@ class Student {
 
 void main(List<String> args) {
   var student = Student('Minh', 21, ['Software Engineering'], 3.5, true, false);
-  // student.name = 'Minh';
-  // student.age = 20;
-  // student.major = ['Software Engineering'];
-  // student.gpa = 3.5;
-  // student.isGraduated = false;
-  // student.isSuspended = false;
-  // print(student.name);
-  // print(student.age);
-  // print(student.major);
-  // print(student.gpa);
-  // print(student.isGraduated);
-  // print(student.isSuspended);
   student.printInfo();
 }
