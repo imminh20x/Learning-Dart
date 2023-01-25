0bd9dc9b28a1e4603930bed5386fadbf41ab6c25
imminh20x <imminh20x@gmail.com>
2023-01-25 21:36:39 +0700

    edit countLettersIS

--- bin/countLettersInString.dart
+++ bin/countLettersInString.dart
@@ -1,11 +1,13 @@
 void main(List<String> args) {
   String content = 'Wellcome to my dart programing';
   List letterList = content.split('');
-  Map<String, int> count = {};
-
+  Map count = {};
   for (String i in letterList) {
+    //Key      Value
     count[i] = i.allMatches(content).length;
   }
-
+  // letterList.forEach(
+  //   (element) => count[element] = element.allMatches(content).length,
+  // );
   print(count);
 }
